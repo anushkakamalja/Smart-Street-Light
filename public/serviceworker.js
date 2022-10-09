db702708b2c0cf5d7c059650bf30f94f3b7f4b8d
ADIVADER19 <advaitnurani@gmail.com>
2022-10-09 16:13:32 +0530

    fixing bug

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,36 +1,36 @@
-const CACHE_NAME = "version-1";
-const urlsToCache = ['index.html', 'offline.html'];
-
-const self= this;
-
-self.addEventListener('install',(event)=>{
-   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache)=>{
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-    })
-   )
-});
-
-self.addEventListener('fetch',(event)=>{
-  event.respondWith(
-    caches.match(event.request).then(()=>{
-        return fetch(event.request).catch(()=>caches.match('offline.html'))
-    })
-  )
-});
-
-self.addEventListener('activate',(event)=>{
-    const cacheWhitelist= [];
-    cacheWhitelist.push(CACHE_NAME);
+/* eslint-disable no-restricted-globals */
+const CACHE_NAME = "version-1"
+const urlstoCache = ["index.html","offline.html"]
+this.addEventListener('install',(event)=>{
     event.waitUntil(
-        caches.keys().then((cacheNames)=> Promise.all(
-            cacheNames.map((cacheName)=>{
-                if(!cacheWhitelist.includes(cacheName))
-                {
-                    return cacheWhitelist.delete(cacheName);
-                }
-            })
-        ))
+        caches.open(CACHE_NAME).then((cache)=>{
+            console.log("Opened Cache");
+            return cache.addAll(urlstoCache);
+        })
     )
-});
+})
+
+
+// listen for request
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+      caches.match(event.request).then((res) => {
+        return fetch(event.request).catch(() => caches.match("offline.html"));
+      })
+    );
+  });
+  
+  // actitivate the service worker
+  self.addEventListener("activate", (event) => {
+      const cacheWhitelist = [];
+      cacheWhitelist.push(CACHE_NAME);
+      event.waitUntil(
+          caches.keys().then((cacheNames) => Promise.all(
+              cacheNames.map((cacheName) => {
+                  if(!cacheWhitelist.includes(cacheName)){
+                      return caches.delete(cacheName);
+                  }
+              })
+          ))
+      )
+  });
