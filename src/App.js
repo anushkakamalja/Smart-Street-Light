f25a3fcb84a7bde8ede850bb5f2352515c0d93dc
ADIVADER19 <advaitnurani@gmail.com>
2022-10-10 11:03:49 +0530

    added useeffect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import {useEffect,useState}  from 'react';
 
 function App() {
+  const [lpg,useLpg]=useState(0);
+  const [co,useCo]=useState(0);
+  const [smoke,useSmoke]=useState(0);
   function noti()
   {
     if (Notification.permission !== 'granted'){
@@ -10,7 +14,7 @@ function App() {
       alert('Allow Notification Please');
      }else {
       var notification = new Notification("Gas level  Notification", {
-       body: "Gas level",
+       body: "Gas level leak at hazardous levels,Open windows and exit the premises immediately",
       });
       notification.onclick = function() {
        window.open('chat.php');
@@ -18,9 +22,13 @@ function App() {
      }
      
   }
-  let lpg=0.0;
-  let co=0.0;
-  let smoke=0.0;
+  useEffect(()=>
+    {
+      if ((co>=65) || (smoke>=45) || (lpg>=550))
+      {
+        noti();
+      }
+    },[]);
   return (
 
     <div className="App">
@@ -30,7 +38,6 @@ function App() {
         <Typography variant="h4">CO Gas level: {co}</Typography>
         <Typography variant="h4">Smoke Level: {smoke}</Typography>
       </div>
-      <Button className="bttn" variant="contained" onClick={noti}>CLICK</Button>
     </div>
   );
 }
