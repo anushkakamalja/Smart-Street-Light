ce2b80dde62b325120fed930a96f9a77a838790c
Gaurav Singh <gaurav.s.140102@gmail.com>
2022-11-14 22:19:58 +0530

    Modify App custom to project

--- src/App.js
+++ src/App.js
@@ -1,34 +1,45 @@
-import './App.css';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import {useEffect,useState}  from 'react';
+import "./App.css";
 
 function App() {
-  const [dat,setDat]=useState(0);
-  const [co,useCo]=useState(0);
-  const [smoke,useSmoke]=useState(0);
-     
-
-  
-  // useEffect(()=>
-  //   {
-  //     useCo(200);
-  //     if ((co>=65) || (smoke>=45) || (lpg>=550))
-  //     {
-  //       noti();
-  //     }
-  //   },[]);
-  return (
-
-    <div className="App">
-      <div className="container">
-        <Typography className="title" variant="h1">GAS KIOSK </Typography>
-        <iframe width="450" height="260" style={{border: "1px solid #cccccc"}} src="https://thingspeak.com/channels/1884399/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"></iframe>
-      </div>
-    </div>
-  );
+	return (
+		<div className="App">
+			<div className="container">
+				<h1 className="title">Smart Street Lighting System</h1>
+				<div className="frameContainer">
+					<iframe
+						width="450"
+						height="260"
+						style={{ border: "1px solid #cccccc" }}
+						src="https://thingspeak.com/channels/1914520/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"
+					></iframe>
+					<iframe
+						width="450"
+						height="260"
+						style={{ border: "1px solid #cccccc" }}
+						src="https://thingspeak.com/channels/1914520/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"
+					></iframe>
+					<iframe
+						width="450"
+						height="260"
+						style={{ border: "1px solid #cccccc" }}
+						src="https://thingspeak.com/channels/1914520/charts/3?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"
+					></iframe>
+					<iframe
+						width="450"
+						height="260"
+						style={{ border: "1px solid #cccccc" }}
+						src="https://thingspeak.com/channels/1914520/charts/5?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"
+					></iframe>
+					<iframe
+						width="450"
+						height="260"
+						style={{ border: "1px solid #cccccc" }}
+						src="https://thingspeak.com/channels/1914520/charts/6?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"
+					></iframe>
+				</div>
+			</div>
+		</div>
+	);
 }
 
-
 export default App;
-
